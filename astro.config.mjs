a2a42fc9ed6bad7f7991bdd4e55adba661d1ed84
Lukáš Chylík <lukas.chylik@riganti.cz>
2024-09-24 21:15:45 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,8 @@ export default defineConfig({
     allowImportingTsExtensions: true,
     integrations: [astroI18next()],
     site: 'https://elendris.github.io/web/',
-    base: '/web',
+    base: '/web/',
+    vite: {
+        base: '/web/',
+    },
 });
