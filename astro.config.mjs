284ea9b27dbdde425e56e91e33178ee8c3b9b20f
Lukáš Chylík <lukas.chylik@riganti.cz>
2024-10-01 13:49:07 +0200

    update pipeline

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import astroI18next from "astro-i18next";
 
+// Determine if the environment is production
+const isProduction = process.env.NODE_ENV === 'production';
+
 // https://astro.build/config
 export default defineConfig({
     allowImportingTsExtensions: true,
     integrations: [astroI18next()],
     site: 'https://elendris.github.io/web/',
-    base: '/web/',
-    vite: {
-        base: '/web/',
-    },
+    base: isProduction ? '/web' : '/',
 });
