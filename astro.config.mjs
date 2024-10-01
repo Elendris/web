d492f17fd9b2c4901a8267d98d672b02752938d8
Lukáš Chylík <lukas.chylik@riganti.cz>
2024-10-01 14:10:22 +0200

    update images path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import astroI18next from "astro-i18next";
 
-// Determine if the environment is production
 const isProduction = process.env.NODE_ENV === 'production';
 
-// https://astro.build/config
 export default defineConfig({
     allowImportingTsExtensions: true,
     integrations: [astroI18next()],
