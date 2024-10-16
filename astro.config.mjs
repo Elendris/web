3662f6395c6ea817162fba08bbd557d380d4b14d
Lukáš Chylík <lukas.chylik@riganti.cz>
2024-10-16 12:20:53 +0200

    Add GTM

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,21 @@
 import { defineConfig } from 'astro/config';
+import partytown from '@astrojs/partytown'
+import sitemap from '@astrojs/sitemap';
 import astroI18next from "astro-i18next";
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
     allowImportingTsExtensions: true,
-    integrations: [astroI18next()],
+    integrations: [
+        astroI18next(),
+        sitemap(),
+        partytown({
+            config: {
+              forward: ["dataLayer.push"],
+            },
+        }),
+    ],
     site: 'https://elendris.github.io/web/',
     base: isProduction ? '/web' : '/',
 });
